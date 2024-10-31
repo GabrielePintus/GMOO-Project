dbc9337e80f4ab29e63ce728729dc0f4e94755f3
Gabriele Pintus <gabrielegpintus@gmail.com>
2024-10-31 12:11:39 +0100

    cleaning

--- run-nlp-plateau.sh
+++
@@ -1,9 +0,0 @@
-python3 script.py \
-    --setup_file setups/setup-nlp-sinewave.py \
-    --dataset datasets/sinewave.parquet \
-    --pop_size 5 \
-    --max_generations 20 \
-    --batch_size 128 \
-    --n_epochs 100 \
-    --tags nlp sinewave plateau prova
-
